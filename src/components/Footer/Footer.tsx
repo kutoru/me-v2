407b06081052a77d5e90e949e101e57753a3aabf
kutoru <eg_bls@mail.ru>
2024-08-28 23:28:18 +0300

    Add icons to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,22 @@
 import "./Footer.css";
+import { ReactComponent as IconGitHub } from "./github.svg";
+import { ReactComponent as IconMail } from "./mail.svg";
 
 export default function Footer() {
   return (
     <div className="footer">
       <div className="separator-big" />
-      <div className="footer-item">
-        <a href="mailto:kutoru.work@gmail.com">kutoru.work@gmail.com</a>
-      </div>
-      <div className="footer-item">
-        Repo:{" "}
+      <div className="footer-item-icon">
         <a href="https://github.com/kutoru/me-v2">
-          https://github.com/kutoru/me-v2
+          <IconGitHub className="icon" />
         </a>
       </div>
-      <div className="footer-item">Hosted via GitHub Pages</div>
+      <div className="footer-item-icon">
+        <a href="mailto:kutoru.work@gmail.com">
+          <IconMail className="icon" />
+        </a>
+      </div>
+      <div className="footer-item-text">Hosted via GitHub Pages</div>
     </div>
   );
 }
